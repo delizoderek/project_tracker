3bc6d4ec9394645d376074b601804d81815b9cb5
Derek DeLizo <delizoderek@gmail.com>
2021-06-30 11:53:52 -0700

    Added a function for creating a new table row and appending to the project table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,37 @@
 let newProjectBtn = $("#create-project");
 let projectTable = $("#project-table");
-console.log("Hello World");
-projectTable.css("background","orange");
+
+let numColumns = 7;
+
+let testProject = {
+    name: "Build a Website",
+    type: "Web Application (Front End)",
+    rate: 100,
+    dueDate: "08/11/2021",
+    daysTilDueDat: 41,
+    earningPotential: 32800
+}
+
+newProjectBtn.on("click",function(event){
+    event.preventDefault();
+    //Create a table row
+    let newTableRow = $("<tr>");
+    //Create a td for each column
+    //populate each td with the correct text
+    for(let el in testProject){
+        let newTd = $("<td>");
+        newTd.text(testProject[el]);
+        newTableRow.append(newTd);
+    }
+    let lastTd = $("<td>");
+    let delBtn = $("<button>");
+    delBtn.text = "X";
+    delBtn.on("click",function(event){
+        event.preventDefault();
+        console.log("You clicked a button");
+    })
+    lastTd.append(delBtn);
+    newTableRow.append(lastTd);
+    projectTable.append(newTableRow);
+
+})
